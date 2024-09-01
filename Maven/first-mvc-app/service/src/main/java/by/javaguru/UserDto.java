b2bb69d197676dc639721dbb8ec14ace5bd76f03
Raizer4 <nikitakraska1@gmail.com>
2024-09-01 21:06:30 +0300

    level-3

---
+++ Maven/first-mvc-app/service/src/main/java/by/javaguru/UserDto.java
@@ -0,0 +1,17 @@
+package by.javaguru;
+
+public class UserDto {
+    private String user;
+
+    public UserDto(String user) {
+        this.user = user;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+}
