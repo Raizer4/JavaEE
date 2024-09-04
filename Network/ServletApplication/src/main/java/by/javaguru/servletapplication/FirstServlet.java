e43d2738ca821add9cf31435102a89624360c583
Raizer4 <nikitakraska1@gmail.com>
2024-09-04 21:41:27 +0300

    Level 5

---
+++ Network/ServletApplication/src/main/java/by/javaguru/servletapplication/FirstServlet.java
@@ -0,0 +1,28 @@
+package by.javaguru.servletapplication;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("INIT");
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("DESTROY");
+    }
+}
