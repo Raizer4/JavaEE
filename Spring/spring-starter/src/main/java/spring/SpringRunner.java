91f4f9bcec0cee070a162d55bdeadbcd4068a96e
Raizer4 <nikitakraska1@gmail.com>
2024-11-04 23:21:38 +0300

    Level - 23-final

--- Spring/spring-starter/src/main/java/spring/SpringRunner.java
+++ Spring/spring-starter/src/main/java/spring/SpringRunner.java
@@ -1,6 +1,8 @@
 package spring;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import spring.dto.UserDto;
+import spring.ioc.Container;
 import spring.mapper.UserMapper;
 import spring.repository.UserRepository;
 import spring.service.UserService;
@@ -8,11 +10,9 @@ import spring.service.UserService;
 public class SpringRunner {
 
     public static void main(String[] args) {
-        var userRepository = new UserRepository();
-        var userDto = new UserDto();
-
-        var userMapper = new UserMapper(userDto);
-        var userService = new UserService(userMapper,userRepository);
+        var context = new ClassPathXmlApplicationContext("application.xml");
+        var userRepository = context.getBean(UserService.class);
+        System.out.println(userRepository);
     }
 
 }
