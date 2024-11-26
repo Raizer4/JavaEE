084d36f02da10888d8bc47d248e6209b31946978
Raizer4 <nikitakraska1@gmail.com>
2024-11-26 22:44:31 +0300

    Spring CRUD

---
+++ Spring/spring-starter/src/main/java/spring/dto/UserCreateEditDto.java
@@ -0,0 +1,20 @@
+package spring.dto;
+
+import lombok.Value;
+import lombok.experimental.FieldNameConstants;
+import org.springframework.format.annotation.DateTimeFormat;
+import spring.database.entity.Role;
+
+import java.time.LocalDate;
+
+@Value
+@FieldNameConstants
+public class UserCreateEditDto {
+    String username;
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    LocalDate birthDate;
+    String firstname;
+    String lastname;
+    Role role;
+    Integer companyId;
+}
---
+++ Spring/spring-starter/src/main/java/spring/mapper/Mapper.java
@@ -0,0 +1,13 @@
+package spring.mapper;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface Mapper<F, T> {
+    T map(F object);
+
+    default T map(F fromObject, T toObject) {
+        return toObject;
+    }
+
+}
