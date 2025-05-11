889cb07c320c311dc924bbfac53378278483b356
Raizer4 <nikitakraska1@gmail.com>
2025-05-11 23:34:31 +0300

    Hibernate

--- Spring/Zayr/spring_course_core/src/main/java/hibernate_test_2/Test1.java
+++ Spring/Zayr/spring_course_core/src/main/java/hibernate_test_2/Test1.java
@@ -1,6 +1,7 @@
 package hibernate_test_2;
 
-import hibernate_test.entity.Employee;
+import hibernate_test_2.entity.Detail;
+import hibernate_test_2.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -10,15 +11,31 @@ public class Test1 {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
                 .buildSessionFactory();
 
         try {
             Session session = factory.getCurrentSession();
-            Employee emp = new Employee("Zaur", "Tregulov"
-                    , "IT", 500);
+
+            Detail detail = Detail.builder()
+                            .city("Toms")
+                            .phone_number("124348")
+                            .email("email.com")
+                            .build();
+
+
+            Employee employee = Employee.builder()
+                    .name("Negr")
+                    .surname("razumist")
+                    .department("It")
+                    .salary(300)
+                    .emdDetail(detail)
+                    .build();
 
             session.beginTransaction();
-            session.save(emp);
+
+            session.save(employee);
+
             session.getTransaction().commit();
         }finally {
             factory.close();
--- Spring/Zayr/spring_course_core/src/main/java/hibernate_test_2/entity/Detail.java
+++ Spring/Zayr/spring_course_core/src/main/java/hibernate_test_2/entity/Detail.java
@@ -1,7 +1,16 @@
 package hibernate_test_2.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "details")
 public class Detail {
